9b4fb7198deaf18c05688eeb20272df0c53bb796
simon <scriado.f@gmail.com>
2022-04-08 09:57:14 +0200

    added more UI elements: tabs, snackbar

--- index.js
+++ index.js
@@ -45,3 +45,67 @@ dropdowns.forEach((dropdown) => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
+
+// Tabs
+class Tabs {
+  constructor(container) {
+    this.container = container;
+    this.tabs = container.querySelectorAll('.trigger');
+  }
+  init() {
+    this.tabs.forEach((tab) => {
+      tab.addEventListener('click', (e) => {
+        if (e.target.tagName === 'LI') {
+          this.toggleTabs(e);
+          this.toggleContent(e);
+        }
+      });
+    });
+  }
+  toggleTabs(e) {
+    // remove current active classes
+    this.tabs.forEach((tab) => tab.classList.remove('active'));
+    // add new active class
+    e.target.classList.add('active');
+  }
+  toggleContent(e) {
+    // remove current active classes
+    this.container.querySelectorAll('.content').forEach((item) => {
+      item.classList.remove('active');
+    });
+    // add new active class
+    const selector = e.target.getAttribute('data-target');
+    const content = this.container.querySelector(selector);
+    content.classList.add('active');
+  }
+}
+
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+// Snackbar
+class Snackbar {
+  constructor() {
+    this.snackbar = document.createElement('div');
+  }
+  init() {
+    this.snackbar.classList.add('snackbar');
+    document.querySelector('body').appendChild(this.snackbar);
+  }
+  show(message) {
+    this.snackbar.textContent = message;
+    this.snackbar.classList.add('active');
+    setTimeout(() => {
+      this.snackbar.classList.remove('active');
+      this.snackbar.textContent = '';
+    }, 4000);
+  }
+}
+
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('you clicked me :)');
+});
